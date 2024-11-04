3231fbb1abaa256d56f3ac71d94dc5ceb89de893
christagabriella <s22210400@student.unklab.ac.id>
2024-11-04 23:39:37 +0800

    christa push

---
+++ src/components/Header/Generation/Gen1/index.js
@@ -0,0 +1,11 @@
+const Gen1 = ({text}) => {
+    return (
+      <>
+      <h1>Gen 1</h1>;
+      <h5>{`${text}`}</h5>
+      </>
+    );
+  };
+  
+  export default Gen1;
+  
---
+++ src/components/Header/Generation/Gen3/index.js
@@ -0,0 +1,11 @@
+const Gen3 = ({text}) => {
+    return (
+      <>
+      <h1>Gen 3</h1>;
+      <h5>{`${text}`}</h5>
+      </>
+    );
+  };
+  
+  export default Gen3;
+  
